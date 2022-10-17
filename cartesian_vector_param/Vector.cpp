29072d289fb2bc096bbeb73cb244d284ff23e955
clement.lavie@edu.esiee.fr <clement.lavie@edu.esiee.fr>
2022-10-17 10:06:48 +0200

    Ajoutez des fichiers projet.

---
+++ cartesian_vector_param/Vector.cpp
@@ -0,0 +1,117 @@
+﻿#include "Vector.hh"
+#pragma once
+
+
+
+#include "config.h"
+
+    // Add suitable constructors
+    Vector::Vector()
+    {
+        for (int i = 0; i < NDIM; i++)
+        {
+            this->coord[i] = 0;
+        }
+         
+    }
+    Vector::Vector(const std::initializer_list<int> IL)
+    {
+        auto Val = std::data(IL);
+        for (int i=0; i < NDIM;i++)
+        {
+            this->coord[i] = Val[i];
+        }
+        
+    }
+
+
+    // possibly more
+    
+        // Public Member functions here
+Vector& Vector::operator+=(const Vector& rhs)
+{
+    for (int i = 0; i < NDIM; i++)
+    {
+        this->coord[i] += rhs.coord[i];
+    }
+    return *this;
+}
+int Vector::operator*(const Vector Vec)
+{
+    int resultat = 0;
+    for (int i = 0; i < NDIM; i++)
+    {
+        resultat += this->coord[i] * Vec.coord[i];
+    }
+    return resultat;
+}
+Vector Vector::operator*(int Nb)
+{
+    Vector newVec = Vector();
+    for (int i = 0; i < NDIM; i++)
+    {
+        newVec.coord[i] = this->coord[i] * Nb;
+    }
+    return newVec;
+}
+Vector& Vector::operator-=(const Vector& rhs)
+{
+    for (int i = 0; i < NDIM; i++)
+    {
+        this->coord[i] -= rhs.coord[i];
+    }
+    return *this;
+}
+Vector& Vector::operator+=(const int nb)
+{
+    for (int i = 0; i < NDIM; i++)
+    {
+        this->coord[i] += nb;
+    }
+    return *this;
+}
+Vector& Vector::operator*=(const int nb)
+{
+    for (int i = 0; i < NDIM; i++)
+    {
+        this->coord[i] *= nb;
+    }
+    return *this;
+}
+    
+Vector Vector::operator+(const Vector rhs)
+{
+    Vector newVec = Vector();
+    for (int i = 0; i < NDIM; i++)
+    {
+        newVec.coord[i] =this->coord[i] + rhs.coord[i];
+    }
+    return newVec;
+}
+Vector Vector::operator-(const Vector rhs)
+{
+    Vector newVec = Vector();
+    for (int i = 0; i < NDIM; i++)
+    {
+        newVec.coord[i] = this->coord[i] - rhs.coord[i];
+    }
+    return newVec;
+}
+
+int& Vector::operator[](size_t i)
+{
+    return this->coord[i];
+}
+
+std::ostream &operator<<(std::ostream& os, const Vector& rhs)
+{
+    os << "{";
+    Vector newVec = Vector(rhs);
+    for (int i = 0; i < NDIM-1; i++)
+    {
+        os << newVec[i]<<",";
+    }
+    os << newVec[NDIM - 1] << "}";
+    return os;
+}
+// Nonmember function operators go here
---
+++ cartesian_vector_param/Vector.hh
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include <ostream>
+#include <initializer_list>
+#include <string>
+#include "config.h"
+
+class Vector
+{
+public:
+    // DO NOT CHANGE THIS
+    Vector(const Vector&) = default;
+    Vector& operator=(const Vector&) = default;
+    ~Vector() = default;
+    //
+
+    // Add suitable constructors
+    Vector();
+    Vector(/*args*/std::initializer_list<int> IL);
+
+    // possibly more
+// Public Member functions here
+    Vector& operator+=(const Vector& rhs);
+    int operator*(const Vector Vec);
+    Vector operator*(int Nb);
+    Vector& operator-=(const Vector& rhs);
+    Vector operator+(const Vector rhs);
+    Vector operator-(const Vector rhs);
+    Vector& operator+=(const int nb);
+    Vector& operator*=(const int nb);
+    int& operator[](size_t i);
+    
+
+    // More to go
+
+private:
+    // Private Member functions here
+    value coord[NDIM];
+    // Member variables are ALWAYS private, and they go here
+};
+std::ostream &operator<<(std::ostream& os, const Vector &rhs);
+// Nonmember function operators go here
